26548490d51099e0bb4ad3b3c8049a2ac4428803
Maxwell Phiri <Maxwell.Phiri@euromonitor.com>
2021-07-11 18:36:37 +0200

    Add project files.

---
+++ Source/StringCalculator.Test/CalculatorTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Xunit;
+
+namespace StringCalculator.Test
+{
+    public class CalculatorTests
+    {
+        private Calculator _sut = new Calculator();
+
+        [Theory]
+        [InlineData("",0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void Add_AddsUpTwoNumbers_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1,3,9", 13)]
+        [InlineData("1,2", 3)]
+        [InlineData("10,20,30", 60)]
+        public void Add_AddsUpTwoAnyNumbers_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1\n3\n9", 13)]
+        [InlineData("1\n2", 3)]
+        public void Add_AddsUpNumbersUsingNewLineDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("//;\n1;\n3;\n9", 13)]
+        [InlineData("//;\n1;\n2;\n4", 7)]
+        public void Add_AddsNumbersUsingCustomDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1,3,-9", "-9")]
+        [InlineData("//;\n-1;\n-2;\n-4", "-1,-2,-4")]
+        public void Add_ShouldThrowException_AddsWithNegativeNumbers(string calculation, string negatives)
+        {
+            //Arrange
+            
+
+            //Act
+            Action action = () => _sut.Add(calculation);
+
+            //Assert
+            var caughtException = Assert.Throws<Exception>(action);
+            Assert.Equal($"Negative numbers not allowed: {negatives}", caughtException.Message);
+        }
+
+        [Theory]
+        [InlineData("10,3000,5", 15)]
+        [InlineData("5,12,1001", 17)]
+        public void Add_ShouldNotAddIfOver1000_AddsLessThan1000Numbers(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("//[***]\n1***3***9", 13)]
+        [InlineData("//[***]\n1***3***4", 8)]
+        public void Add_AddsNumbersUsingMultiDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("//[*][%]\n1*3%9", 13)]
+        [InlineData("//[*][%]\n1*3%4", 8)]
+        public void Add_AddsNumbersUsingMultipleDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("//[***][-%]\n1***3-%9", 13)]
+        [InlineData("//[***][-%]\n1***3-%4", 8)]
+        public void Add_AddsNumbersUsingMultiLengthDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.Add(calculation);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Source/StringCalculator/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            var delimiters = new List<char> { ',', '\n' };
+
+            if (numbers.Contains("//"))
+            {
+                var customSplit = numbers.Replace("//", string.Empty).Split('\n', 2);
+
+                if (customSplit[0].Contains("["))
+                {
+                    var multiDelimiter = customSplit[0].Split(new char[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string splitChar in multiDelimiter)
+                    {
+                        delimiters.AddRange(splitChar.ToCharArray());
+                    }
+                }
+                else
+                {
+                    var customDelimiter = customSplit[0].SingleOrDefault();
+                    delimiters.Add(customDelimiter);
+                }
+
+                numbers = customSplit[1];
+            }
+
+            var splitNumbers = numbers
+                                .Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .Where(x => x < 1000)
+                                .ToList();
+
+            var negativeNumbers = splitNumbers.Where(x => x < 0).ToList();
+
+            if (negativeNumbers.Any())
+            {
+                throw new Exception("Negative numbers not allowed: " + string.Join(",",negativeNumbers));
+            }
+
+            return splitNumbers.Sum();
+        }
+    }
+}
